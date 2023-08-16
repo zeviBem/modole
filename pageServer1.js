8cf52c7fbd02ad63be9af9ffb2f199a0d0b9528b
zevi bem <zevibem@gmail.com>
2023-08-16 10:18:09 +0300

    add a new section num 4

--- pageServer1.js
+++ pageServer1.js
@@ -1,14 +1,15 @@
 function createId() {
-    const {v4: uuidv4} = require('uuid')
+    const { v4: uuidv4 } = require('uuid')
     return uuidv4()
 }
-// id1 = createId()
-// id1 = createId()
-// id1 = createId()
+id1 = '2cbe5d7d-d2a2-410d-a199-62722eb1e17b';
+id2 = '961dc7bd-835a-4d06-a2f5-d2ab340a3171';
+id3 = "501b46c1-ee18-4d05-a3eb-a30744b50d5b";
+
 const users = [
-    {id: 1, name: 'zev', password: '1234'},
-    {id: 2, name: 'trump', password: '2134'},
-    {id: 3, name: 'ari', password: '3214'}
+    { id: id1, email: 'zev@', password: '1111' },
+    { id: id2, email: 'trump@', password: '2222' },
+    { id: id3, email: 'ari@', password: '3333' }
 ]
 
 const express = require('express')
@@ -16,24 +17,27 @@ const app = express()
 const port = 4000
 app.use(express.json())
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send(users)
 })
-app.get('/users/:id',(req, res) => {
+app.get('/users/:id', (req, res) => {
     let bool = 0;
-    console.log(req.params.id)
     users.forEach(element => {
         if (element.id == req.params.id) {
             res.send(element)
-            bool ++;
-         }
+            bool++;
+        }
     })
     if (!bool) {
         res.send('this id is')
-     }
+    }
 })
-app.post('/users',(req, res) => {
-    const newUser = {id: req.body.id, name: req.body.name, password: req.body.password}
+app.post('/users', (req, res) => {
+
+    const newUser = { id: createId(),
+                      name: req.body.name,
+                     password: req.body.password
+                    }
     console.log(newUser)
     users.push(newUser)
     res.send(users)
@@ -55,14 +59,14 @@ app.put('/users/', (req, res) => {
     }
 })
 
-app.delete('/users/:id',(req, res) => {
+app.delete('/users/:id', (req, res) => {
     const deleteUser = req.params.id
     let count = 0
     users.forEach((element, index) => {
         if (element.id == deleteUser) {
             users.splice(index, 1)
             res.send(users)
-            count ++;
+            count++;
         }
     })
     if (!count) {
@@ -70,8 +74,23 @@ app.delete('/users/:id',(req, res) => {
     }
 })
 
+app.post("/users//", (req, res) => {
+    const userEmail = req.body.email;
+    const userPas = req.body.password;
+    let answer = false
+    users.forEach(element => {
+        if ((element.email == userEmail) & (element.password == userPas)) {
+            res.send('ERROR!! this user its create before you!')
+            answer = true
+        }
+    })
+    if (!answer) {
+        res.send('this is correct!')
+    }
+})
 
 
-app.listen(port,() => {
+
+app.listen(port, () => {
     console.log(`server is ap and running on port ${port}`)
 })
